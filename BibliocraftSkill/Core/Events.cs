868405ae71347ff19f4fa3d187cabc6b60244ca1
Pet-Slime <52937167+Pet-Slime@users.noreply.github.com>
2024-11-11 16:13:07 -0600

    Fixed issues with Biblocraft preventing it being added

---
+++ BibliocraftSkill/Core/Events.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirbCore.Attributes;
+using BookSkill;
+using MoonShared.APIs;
+using Netcode;
+using SpaceCore;
+using SpaceCore.Events;
+using SpaceShared.APIs;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Events;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using StardewValley.Quests;
+using StardewValley.TerrainFeatures;
+
+namespace BookSkillCode.Core
+{
+    [SEvent]
+    internal class Events
+    {
+        [SEvent.GameLaunchedLate]
+        private static void GameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            Log.Warn("Bibliocraft: Trying to Register skill.");
+            SpaceCore.Skills.RegisterSkill(new Book_Skill());
+
+
+        }
+    }
+}
