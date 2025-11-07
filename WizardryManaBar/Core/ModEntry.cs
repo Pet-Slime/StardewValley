c68c3b18cc7da1e98c8ccb8bb017c5f8d1d5ade7
Bell Mcbreen <time.dragoon@live.com>
2025-11-07 15:57:09 -0600

    New Trigger actions for Mana bar API

--- WizardryManaBar/Core/ModEntry.cs
+++ WizardryManaBar/Core/ModEntry.cs
@@ -4,6 +4,8 @@ using Microsoft.Xna.Framework.Graphics;
 using MoonShared.Attributes;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Triggers;
 
 namespace WizardryManaBar.Core
 {
@@ -27,6 +29,22 @@ namespace WizardryManaBar.Core
             MoonShared.Attributes.Parser.InitEvents(helper);
             MoonShared.Attributes.Parser.ParseAll(this);
             ModEntry.Instance.Helper.Events.GameLoop.GameLaunched += Events.GameLaunched;
+
+            TriggerActionManager.RegisterAction(
+                $"moonslime.ManaBarAPI.AddMana",
+                AddMana);
+
+            TriggerActionManager.RegisterAction(
+                $"moonslime.ManaBarAPI.SetMaxMana",
+                SetMaxMana);
+
+            TriggerActionManager.RegisterAction(
+                $"moonslime.ManaBarAPI.AddToMaxMana",
+                AddToMaxMana);
+
+            TriggerActionManager.RegisterAction(
+                $"moonslime.ManaBarAPI.SetManaToMax",
+                SetManaToMax);
         }
 
         public override object GetApi()
@@ -41,5 +59,47 @@ namespace WizardryManaBar.Core
             }
 
         }
+
+        static bool AddMana(string[] args, TriggerActionContext context, out string error)
+        {
+            if (!ArgUtility.TryGetInt(args, 1, out int points, out error, "int points"))
+            {
+                return false;
+            }
+            Game1.player.AddMana(points);
+            return true;
+        }
+
+        static bool SetMaxMana(string[] args, TriggerActionContext context, out string error)
+        {
+            if (!ArgUtility.TryGetInt(args, 1, out int points, out error, "int points"))
+            {
+                return false;
+            }
+            Game1.player.SetMaxMana(points);
+            return true;
+        }
+
+        static bool AddToMaxMana(string[] args, TriggerActionContext context, out string error)
+        {
+            if (!ArgUtility.TryGetInt(args, 1, out int points, out error, "int points"))
+            {
+                return false;
+            }
+            Game1.player.SetMaxMana(Game1.player.GetMaxMana() + points);
+            return true;
+        }
+
+        static bool SetManaToMax(string[] args, TriggerActionContext context, out string error)
+        {
+            if (!ArgUtility.TryGetInt(args, 1, out int points, out error, "int points"))
+            {
+                return false;
+            }
+            Game1.player.SetManaToMax();
+            return true;
+        }
+
+
     }
 }
